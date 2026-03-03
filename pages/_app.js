4664c7e4b35b95790b7adf45a5d42d137a9ee250
erikdkrs-cloud <erik.dkrs@gmail.com>
2026-03-03 18:38:16 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import "../styles/globals.css";
+// pages/_app.js
+import "@/styles/globals.css";
+import "@/styles/dkrs-theme.css";
+import { Sora } from "next/font/google";
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const sora = Sora({
+  subsets: ["latin", "cyrillic"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
+
+export default function App({ Component, pageProps }) {
+  return (
+    <div className={sora.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
