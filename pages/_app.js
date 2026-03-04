1e8dcdb312b1c71c734253f4f4ab54c46f5a08b6
erikdkrs-cloud <erik.dkrs@gmail.com>
2026-03-04 15:36:52 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 // pages/_app.js
 import "../styles/globals.css";
-import "../styles/dkrs-theme.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
