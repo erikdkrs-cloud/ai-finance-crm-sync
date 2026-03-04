b46d2aa597e9aa037ee27a6f815e3ba66ddd3994
erikdkrs-cloud <erik.dkrs@gmail.com>
2026-03-04 14:11:34 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,7 @@
 // pages/_app.js
 import "../styles/globals.css";
 import "../styles/dkrs-theme.css";
-import { Inter } from "next/font/google";
-
-const inter = Inter({
-  subsets: ["latin", "cyrillic"],
-  weight: ["400", "500", "600", "700"],
-  display: "swap",
-});
 
 export default function App({ Component, pageProps }) {
-  return (
-    <div className={`${inter.className} dkrs-root dkrs-bg`}>
-      <Component {...pageProps} />
-    </div>
-  );
+  return <Component {...pageProps} />;
 }
